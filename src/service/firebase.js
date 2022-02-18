9e9f8659e98b08117ea01d2afd186df27fde55c4
五十嵐渉 <>
2022-02-18 17:55:20 +0900

    Initial commit

---
+++ src/service/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCJyeUscgWXE9jes6L7HSmS38OqvKJDGVQ",
+    authDomain: "hometask-app.firebaseapp.com",
+    projectId: "hometask-app",
+    storageBucket: "hometask-app.appspot.com",
+    messagingSenderId: "528919654485",
+    appId: "1:528919654485:web:9afe8d5d179471754da4dc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
